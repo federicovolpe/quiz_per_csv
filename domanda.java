7fccc0a2c0322681c6e0bb70bf55af74b4997d17
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-06-12 10:30:30 +0200

    iz working

--- domanda.java
+++ domanda.java
@@ -1,20 +1,23 @@
 public class domanda {
-    String numero;
-    String domanda;
-    String opzione1;
-    String opzione2;
-    String opzione3;
-    String opzione4;
-    String risposta;
+    public String numero;
+    public String domanda;
+    public String[] opzioni = new String[5];
+    public String risposta;
     
     //costruttore
     public domanda(String numero,String domanda, String opzione1, String opzione2, String opzione3, String opzione4, String risposta){
         this.numero = numero;
         this.domanda = domanda;
-        this.opzione1 = opzione1;
-        this.opzione2 = opzione2;
-        this.opzione3 = opzione3;
-        this.opzione4 = opzione4;
+        this.opzioni[1] = opzione1;
+        this.opzioni[2] = opzione2;
+        this.opzioni[3] = opzione3;
+        this.opzioni[4] = opzione4;
         this.risposta = risposta;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Domanda numero: " + numero + "\nDomanda: " + domanda + "\nOpzione 1: " + opzioni[1] + "\nOpzione 2: " + opzioni[2] + "\nOpzione 3: " + opzioni[3] + "\nOpzione 4: " + opzioni[4] + "\n";
+        return s;
     }
 }
--- main.java
+++ main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class main{
     public static void main(String[] args){
@@ -10,19 +12,56 @@ public class main{
         String csvFile = "./nuovo.csv";
         String line;
         String csvSeparator = ";"; // Specify the separator used in your CSV file
+        int conta_corrette = 0;
+        int conta_sbagliate = 0;
 
+        ArrayList<domanda> domande = new ArrayList<domanda>();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
                 //arry di stringhe con i dati
                 String[] data = line.split(csvSeparator);
                 
-                //creazione array di domande
-                ArrayList<domanda> domande = new ArrayList<domanda>();
+                //riempimento dell'arraylist
                 domande.add(new domanda(data[0],data[1],data[2],data[3],data[4],data[5],data[6]));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("le domande sono state caricate con successo")
+        System.out.println("le domande sono state caricate con successo");
+        
+        //selezione di una domanda a caso
+        //while tthe arraylist is not empty
+        while(domande.size() > 0){
+            //generazione di un numero casuale
+            Random rand = new Random();
+            int n = rand.nextInt(domande.size());
+            
+            //stampa della domanda
+            System.out.println(domande.get(n).toString());
+            
+            //read next int
+            Scanner in = new Scanner(System.in);
+            int selezione = in.nextInt();
+            
+            //se la risposta è giusta
+            if(domande.get(n).opzioni[selezione].equals(domande.get(n).risposta)){
+                System.out.println("la risposta è corretta");
+
+                //rimuovo la domanda dall'arraylist
+                domande.remove(n);
+
+                //incremento il contatore delle risposte corrette
+                conta_corrette++;
+
+            }else{
+                System.out.println("la risposta è sbagliata,la tua risposta:"+ domande.get(n).opzioni[selezione] +"\n risposta corretta: " + domande.get(n).risposta + "\n");   
+                //non viene tolta la domanda cosi puoi riprovare
+
+                conta_sbagliate++;
+            }
+            
+            //rimozione della domanda
+            domande.remove(n);
+        }
     }
 }
